60d7630b8a91133702bf80f74223969b4769d42a
saisrikar-surisetti <surisetti410@gmail.com>
2025-01-01 05:43:29 -0700

    struggling with eslint

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/app/lib/db";
 import { getCurrentUser } from "@/app/lib/session";
+
 import { NextResponse } from "next/server";
 
 export async function POST(req:Request) {
@@ -20,6 +21,8 @@ export async function POST(req:Request) {
          return NextResponse.json({newPost}, {status: 200} )
 
     } catch (error){
+    
         return NextResponse.json({message: "something went wrong!"}, {status: 500})
     }
+      
 }
